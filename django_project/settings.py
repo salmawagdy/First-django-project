68b7b60d0666e9e8e4a422f5feb3d87092eee49e
Your Name <you@example.com>
2026-02-11 00:38:18 +0200

    profile updated

--- django_project/settings.py
+++ django_project/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
